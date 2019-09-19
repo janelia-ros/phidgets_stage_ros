60dc89242440a034a774fa807e76f6af0fe32959
Peter Polidoro <peterpolidoro@gmail.com>
2019-09-19 16:25:49 -0400

    Add nodes to launch

--- phidgets_stage/launch/stage.launch.py
+++ phidgets_stage/launch/stage.launch.py
@@ -14,9 +14,21 @@ def generate_launch_description():
                 ComposableNode(
                     package='phidgets_digital_inputs',
                     node_plugin='phidgets::DigitalInputsRosI',
-                    node_name='phidgets_digital_inputs',
-                    parameters=[{'serial':-1,'hub_port':0,'is_hub_port_device':True,'publish_rate':0}],
+                    node_name='phidgets_digital_inputs1',
+                    parameters=[{'serial':-1,'hub_port':1,'is_hub_port_device':True,'publish_rate':0}],
                 ),
+                ComposableNode(
+                    package='phidgets_digital_inputs',
+                    node_plugin='phidgets::DigitalInputsRosI',
+                    node_name='phidgets_digital_inputs3',
+                    parameters=[{'serial':-1,'hub_port':3,'is_hub_port_device':True,'publish_rate':0}],
+                ),
+                # ComposableNode(
+                #     package='phidgets_motors',
+                #     node_plugin='phidgets::MotorsRosI',
+                #     node_name='phidgets_motors',
+                #     parameters=[{'serial':-1,'hub_port':0,'braking_strength':0.0,'data_interval_ms':250,'publish_rate':0}],
+                # ),
             ],
             output='screen',
     )
